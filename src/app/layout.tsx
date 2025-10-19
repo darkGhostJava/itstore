78ce11285192637d042053bb914604fcbdd4e76c
Abd <abd804027@gmail.com>
2025-10-19 07:55:01 +0000

    there is decalage between the side bar and dashboard

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,11 +42,11 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-            <div className="flex min-h-screen w-full flex-col">
+            <div className="flex min-h-screen w-full">
               <Sidebar />
-              <div className="flex flex-col sm:pl-14">
+              <div className="flex flex-1 flex-col sm:pl-14">
                 <Header />
-                <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+                <main className="flex-1 p-4 sm:p-6 md:p-8">
                   {children}
                 </main>
               </div>
