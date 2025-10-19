0d6379846b09cfca0c69e38945123a58cf8556f5
Abd <abd804027@gmail.com>
2025-10-19 07:57:24 +0000

    the same problem

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,16 +42,16 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-            <div className="flex min-h-screen w-full">
-              <Sidebar />
-              <div className="flex flex-col flex-1 w-full sm:ml-14">
-                <Header />
-                <main className="flex-1 p-4 sm:p-6 md:p-8">
-                  {children}
-                </main>
-              </div>
+          <div className="relative flex min-h-screen w-full">
+            <Sidebar />
+            <div className="flex flex-1 flex-col sm:pl-14">
+              <Header />
+              <main className="flex-1 p-4 sm:p-6 md:p-8">
+                {children}
+              </main>
             </div>
-            <Toaster />
+          </div>
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>
