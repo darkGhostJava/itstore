551d43320f849e0df2e04c0884e7c70d4ee861fc
Abd <abd804027@gmail.com>
2025-12-23 15:37:41 +0000

    thank u so match no i want to add the a button to show all item for this

--- src/app/structures/structure-tree.tsx
+++ src/app/structures/structure-tree.tsx
@@ -108,7 +108,7 @@ export function StructureTree({ structure, isRoot = true, onBack }: StructureTre
                   <DropdownMenuLabel>Actions</DropdownMenuLabel>
                   {canNavigate && (
                   <DropdownMenuItem asChild>
-                      <Link href={`/structures/${linkId}`}>View Details</Link>
+                      <Link href={`/structures/${linkId}`}>View Items</Link>
                   </DropdownMenuItem>
                   )}
                   <DropdownMenuItem>Assign Chef</DropdownMenuItem>
