73f73198facc519ba8dc5edd0fc90a217e261af8
Abd <abd804027@gmail.com>
2025-12-24 13:53:46 +0000

    thanks

--- src/components/data-table/data-table.tsx
+++ src/components/data-table/data-table.tsx
@@ -190,7 +190,7 @@ export function DataTable<TData, TValue>({
                   colSpan={columns.length}
                   className="h-24 text-center"
                 >
-                  No results.
+                  No data available.
                 </TableCell>
               </TableRow>
             )}
